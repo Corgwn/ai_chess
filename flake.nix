96f1d95fd9cdad76d073f066be7a3f818b2af523
Corgwn <ryan.duffendack@gmail.com>
2025-10-14 17:57:06 -0500

    updated to naersk build tools

--- flake.nix
+++ flake.nix
@@ -1,16 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
     { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      naerskLib = pkgs.callPackage naersk { };
     in
     {
 
-      packages."x86_64-linux".default = pkgs.callPackage ./default.nix { };
+      packages."x86_64-linux".default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = [ pkgs.glib ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
 
       devShells."x86_64-linux".default = pkgs.mkShell {
         buildInputs = with pkgs; [
