f53298823b5f7cf151591d613f281c13c057b328
Corgwn <ryan.duffendack@gmail.com>
2025-10-15 08:41:56 -0500

    added naersk managing cargo

--- flake.nix
+++ flake.nix
@@ -2,33 +2,51 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     naersk.url = "github:nix-community/naersk";
+    # fenix = {
+    #   url = "github:nix-community/fenix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      naersk,
+    # fenix,
+    }:
     let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
       naerskLib = pkgs.callPackage naersk { };
+      # fenixLib = fenix.packages."x86_64-linux";
+      # rustToolchain = fenixLib.stable.toolchain;
     in
     {
 
+      # (naerskLib.override {cargo=rustToolchain;rustc=rustToolchain;}).buildPackage
       packages."x86_64-linux".default = naerskLib.buildPackage {
         src = ./.;
-        buildInputs = [ pkgs.glib ];
+        buildInputs = [
+          pkgs.glib
+          pkgs.openssl
+          pkgs.sqlite
+          pkgs.gcc
+        ];
         nativeBuildInputs = [ pkgs.pkg-config ];
       };
 
       devShells."x86_64-linux".default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-          rustc
-          rustfmt
-          clippy
-          rust-analyzer
-          glib
+        buildInputs = [
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.openssl
+          pkgs.sqlite
+          pkgs.gcc
         ];
 
-        nativeBuildInputs = [ pkgs.pkg-config ];
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
 
         env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
